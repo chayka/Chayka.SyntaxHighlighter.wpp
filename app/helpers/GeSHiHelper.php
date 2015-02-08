3514f99114fface018bdc132b6e500e7976a0bf3
Boris Mossounov <borix@tut.by>
2015-02-08 15:20:30 +0300

    Minor fixes

--- app/helpers/GeSHiHelper.php
+++ app/helpers/GeSHiHelper.php
@@ -27,29 +27,48 @@ class GeSHiHelper {
 	public static function highlight($code, $lang){
 		$geshi = new GeSHi($code, $lang);
 		$geshi->enable_classes();
-		self::registerHighlighter($lang, $geshi);
+		$styles = self::registerHighlighter($lang, $geshi);
+		$geshi->set_overall_class('code geshi');
+		$code = $geshi->parse_code();
+		$code =preg_replace('%(<pre class="[^"]*">)(&nbsp;)?\s*%m', '$1', $code);
+		return $styles.$code;
+	}
 
-		return $geshi->parse_code();
+	/**
+	 * Get all the needed styles in the form of
+	 * <style type="text/css" id="geshi_lang_...">...</style>
+	 *
+	 * @return string
+	 */
+	public static function getStyles(){
+		$styles = '';
+		foreach(self::$stylesheets as $lang => $geshi){
+			$styles.=sprintf('<style type="text/css" id="geshi_lang_%s">%s</style>', $lang, $geshi->get_stylesheet());
+		}
+		return $styles;
 	}
 
 	/**
 	 * Output inline html styles for the rendered code blocks
 	 */
 	public static function renderStyles(){
-		foreach(self::$stylesheets as $lang => $geshi){
-			printf('<style type="text/css" id="geshi_lang_%s">%s</style>', $lang, $geshi->get_stylesheet());
-		}
+		echo self::getStyles();
 	}
 
 	/**
 	 * @param string $lang
 	 * @param GeSHi $geshi
+	 *
+	 * @return string
 	 */
 	public static function registerHighlighter($lang, $geshi){
+		$styles = '';
 		if(!count(self::$stylesheets)){
-			add_action('wp_head', function(){self::renderStyles();});
+//			add_filter('the_content', function($content){self::getStyles().$content;});
+			$styles = sprintf('<style type="text/css" id="geshi_lang_%s">%s</style>', $lang, $geshi->get_stylesheet());
 		}
 		self::$stylesheets[$lang] = $geshi;
+		return $styles;
 	}
 
 }
